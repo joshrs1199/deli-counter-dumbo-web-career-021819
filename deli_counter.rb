59bde951bb4271d47edb8dfddf9dcd2529cf5da2
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 21:55:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,3 +3,12 @@ def take_a_number(line,name)
   line.push(name)
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
+def line(deli_line)
+  if deli_line.length == 0
+    puts "The line is currently empty."
+  else
+    Katz_line = "The line is currently:"
+    deli_line.each_with_index do |name,index|
+      Katz_line += "#{index + 1}. #{name}"
+end
